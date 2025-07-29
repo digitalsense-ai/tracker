983f300127234d7c9bc4d2e824bbd4d4145ab042
digitalsense-ai <jan.dalsager@gmail.com>
2025-07-29 22:11:06 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,3 +2,6 @@ protected function schedule(Schedule $schedule)
 {
     $schedule->command('forecast:refresh')->everyFiveMinutes();
 }
+protected $commands = [
+    \App\Console\Commands\SimulateTrades::class,
+];
