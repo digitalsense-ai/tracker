78f814176db7bf828785d6df053f83299eac9e99
digitalsense-ai <jan.dalsager@gmail.com>
2025-07-29 22:17:21 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,3 +5,7 @@ protected function schedule(Schedule $schedule)
 protected $commands = [
     \App\Console\Commands\SimulateTrades::class,
 ];
+protected function schedule(Schedule $schedule)
+{
+    $schedule->command('simulate:trades')->dailyAt('15:40'); // fx 15:40 dansk tid
+}
