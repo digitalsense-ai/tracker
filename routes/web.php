13a910d5af22bc3e89ac85325512abb059abc1d6
digitalsense-ai <jan.dalsager@gmail.com>
2025-08-03 13:34:51 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,1 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ExplainerController;
-use App\Http\Controllers\TradeResultController;
-
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::post('/update-forecast-config', [DashboardController::class, 'updateForecastConfig']);
-Route::get('/explainer', [ExplainerController::class, 'index']);
-Route::get('/results', [TradeResultController::class, 'index']);
-
-use App\Http\Controllers\BacktestController;
-Route::get('/backtest', [BacktestController::class, 'index']);
+// Placeholder for routes/web.php
