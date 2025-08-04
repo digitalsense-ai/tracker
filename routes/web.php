8d31215c80e83e31d1a8839a7a894d2860e19214
digitalsense-ai <jan.dalsager@gmail.com>
2025-08-04 20:22:26 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExplainerController;
 use App\Http\Controllers\TradeResultController;
+use App\Http\Controllers\BacktestController;
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/explainer', [ExplainerController::class, 'index'])->name('explainer');
 
 Route::get('/results', [TradeResultController::class, 'index'])->name('results');
+
+Route::get('/backtest', [BacktestController::class, 'index'])->name('backtest');
+
