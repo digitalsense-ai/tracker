9ff6a43d5c2a53df9e1fbcd51bb96996f889c568
digitalsense-ai <jan.dalsager@gmail.com>
2025-08-04 19:05:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,16 @@
-// Final version 3 placeholder: routes/web.php
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExplainerController;
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\BacktestController;
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/explainer', [ExplainerController::class, 'index'])->name('explainer');
+Route::get('/results', [ResultController::class, 'index'])->name('results');
+Route::get('/backtest', [BacktestController::class, 'index'])->name('backtest');
