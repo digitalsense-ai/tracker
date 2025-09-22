4cc4bbc99b631d7692947656706f1b00b5f690da
Prem Kalyan <premk27@gmail.com>
2025-09-22 16:20:36 +0000

    server changes - FINISH_LINE

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('forecast:refresh')->everyFiveMinutes();
         $schedule->command('simulate:trades')->dailyAt('15:40'); // fx 15:40 dansk tid
-        $schedule->command('profiles:recompute')->weekdays()->at('17:15');
+        //$schedule->command('profiles:recompute')->weekdays()->at('17:15');
+        $schedule->command('profiles:recompute')->cron('3 * * * *');
     }
 
     /**
